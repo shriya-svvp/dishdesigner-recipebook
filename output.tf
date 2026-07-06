72d03db71be90ecd587cab81a93e5583c12a098c
Shriya M.V <shriyamv28@gmail.com>
2026-07-06 09:44:59 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "file_name" {
+  value = local_file.sample_file.filename
+}
+output "file_content" {
+  value = local_file.sample_file.content
+}
+output "file_id" {
+  value = local_file.sample_file.id
+}
